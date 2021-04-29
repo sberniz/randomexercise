ff195dfafb2a5a58cc8155cbfd73b4223fa5023b
Santiago Berniz <sberniz@gmail.com>
2021-04-29 12:17:33 -0700

    Removed Google          Ads script

--- index.php
+++ index.php
@@ -6,21 +6,6 @@
 </head>
 <body>
 <p>Click Open App answer the prompts(24 hours mode for ex 3pm is 15) and Enjoy</p>
-
-<div id="Google_Ads1">
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9264436149757040"
-     crossorigin="anonymous"></script>
-<!-- RandoM_exercise_app -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-9264436149757040"
-     data-ad-slot="3537973844"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
 <button onclick="openexercise()" type="button">Random Exercise WebApp</button>
 
 <script language="Javascript">
